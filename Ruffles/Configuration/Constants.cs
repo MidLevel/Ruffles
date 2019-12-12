d74f325b3d791ea8c12906c4395eb70049cf75da
Albin Corén <twoten@twoten.io>
2019-12-12 15:23:12 +0100

    feat: Added protocol identification to filter unwanted garbage data

---
+++ Ruffles/Configuration/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Ruffles.Configuration
+{
+    internal static class Constants
+    {
+        // 7x NULL | Ruffles Greets You | 7x NULL
+        internal static byte[] RUFFLES_PROTOCOL_IDENTIFICATION = new byte[32] { 00, 00, 00, 00, 00, 00, 00, 82, 117, 102, 102, 108, 101, 115, 32, 71, 114, 101, 101, 116, 115, 32, 89, 111, 117, 00, 00, 00, 00, 00, 00, 00 };
+    }
+}
