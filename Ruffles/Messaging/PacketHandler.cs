31473c192c36cd24142a47797fa84781df6ee4aa
Albin Corén <twoten@twoten.io>
2019-12-17 08:27:38 +0100

    fix: Added safety check for dead channels

--- Ruffles/Messaging/PacketHandler.cs
+++ Ruffles/Messaging/PacketHandler.cs
@@ -24,54 +24,63 @@ namespace Ruffles.Messaging
                 return;
             }
 
-            HeapPointers incomingPointers = connection.Channels[channelId].HandleIncomingMessagePoll(new ArraySegment<byte>(payload.Array, payload.Offset + 1, payload.Count - 1), out byte headerBytes);
+            IChannel channel = connection.Channels[channelId];
 
-            if (incomingPointers != null)
+            if (channel != null)
             {
-                // There is new packets
-                for (int i = 0; i < incomingPointers.VirtualCount; i++)
+                HeapPointers incomingPointers = channel.HandleIncomingMessagePoll(new ArraySegment<byte>(payload.Array, payload.Offset + 1, payload.Count - 1), out byte headerBytes);
+
+                if (incomingPointers != null)
                 {
-                    MemoryWrapper wrapper = (MemoryWrapper)incomingPointers.Pointers[i];
-
-                    HeapMemory memory = null;
-
-                    if (wrapper.AllocatedMemory != null)
+                    // There is new packets
+                    for (int i = 0; i < incomingPointers.VirtualCount; i++)
                     {
-                        memory = wrapper.AllocatedMemory;
-                    }
+                        MemoryWrapper wrapper = (MemoryWrapper)incomingPointers.Pointers[i];
 
-                    if (wrapper.DirectMemory != null)
-                    {
-                        // Alloc memory
-                        memory = memoryManager.AllocHeapMemory((uint)wrapper.DirectMemory.Value.Count);
+                        HeapMemory memory = null;
 
-                        // Copy payload to borrowed memory
-                        Buffer.BlockCopy(wrapper.DirectMemory.Value.Array, wrapper.DirectMemory.Value.Offset, memory.Buffer, 0, wrapper.DirectMemory.Value.Count);
-                    }
-
-                    if (memory != null)
-                    {
-                        // Send to userspace
-                        connection.Socket.PublishEvent(new NetworkEvent()
+                        if (wrapper.AllocatedMemory != null)
                         {
-                            Connection = connection,
-                            Socket = connection.Socket,
-                            Type = NetworkEventType.Data,
-                            AllowUserRecycle = true,
-                            Data = new ArraySegment<byte>(memory.Buffer, (int)memory.VirtualOffset, (int)memory.VirtualCount),
-                            InternalMemory = memory,
-                            SocketReceiveTime = NetTime.Now,
-                            ChannelId = channelId,
-                            MemoryManager = memoryManager
-                        });
+                            memory = wrapper.AllocatedMemory;
+                        }
+
+                        if (wrapper.DirectMemory != null)
+                        {
+                            // Alloc memory
+                            memory = memoryManager.AllocHeapMemory((uint)wrapper.DirectMemory.Value.Count);
+
+                            // Copy payload to borrowed memory
+                            Buffer.BlockCopy(wrapper.DirectMemory.Value.Array, wrapper.DirectMemory.Value.Offset, memory.Buffer, 0, wrapper.DirectMemory.Value.Count);
+                        }
+
+                        if (memory != null)
+                        {
+                            // Send to userspace
+                            connection.Socket.PublishEvent(new NetworkEvent()
+                            {
+                                Connection = connection,
+                                Socket = connection.Socket,
+                                Type = NetworkEventType.Data,
+                                AllowUserRecycle = true,
+                                Data = new ArraySegment<byte>(memory.Buffer, (int)memory.VirtualOffset, (int)memory.VirtualCount),
+                                InternalMemory = memory,
+                                SocketReceiveTime = NetTime.Now,
+                                ChannelId = channelId,
+                                MemoryManager = memoryManager
+                            });
+                        }
+
+                        // Dealloc the wrapper
+                        memoryManager.DeAlloc(wrapper);
                     }
 
-                    // Dealloc the wrapper
-                    memoryManager.DeAlloc(wrapper);
+                    // Dealloc the pointers
+                    memoryManager.DeAlloc(incomingPointers);
                 }
-
-                // Dealloc the pointers
-                memoryManager.DeAlloc(incomingPointers);
+            }
+            else
+            {
+                if (Logging.CurrentLogLevel <= LogLevel.Warning) Logging.LogWarning("Receive packet failed because the channel is not assigned");
             }
         }
 
@@ -84,26 +93,33 @@ namespace Ruffles.Messaging
 
             IChannel channel = connection.Channels[channelId];
 
-            HeapPointers memoryPointers = channel.CreateOutgoingMessage(payload, out byte headerSize, out bool dealloc);
-
-            if (memoryPointers != null)
+            if (channel != null)
             {
-                for (int i = 0; i < memoryPointers.VirtualCount; i++)
-                {
-                    connection.SendRaw(new ArraySegment<byte>(((HeapMemory)memoryPointers.Pointers[i]).Buffer, (int)((HeapMemory)memoryPointers.Pointers[i]).VirtualOffset, (int)((HeapMemory)memoryPointers.Pointers[i]).VirtualCount), noMerge, headerSize);
-                }
+                HeapPointers memoryPointers = channel.CreateOutgoingMessage(payload, out byte headerSize, out bool dealloc);
 
-                if (dealloc)
+                if (memoryPointers != null)
                 {
-                    // DeAlloc the memory again. This is done for unreliable channels that dont need the message after the initial send.
                     for (int i = 0; i < memoryPointers.VirtualCount; i++)
                     {
-                        memoryManager.DeAlloc(((HeapMemory)memoryPointers.Pointers[i]));
+                        connection.SendRaw(new ArraySegment<byte>(((HeapMemory)memoryPointers.Pointers[i]).Buffer, (int)((HeapMemory)memoryPointers.Pointers[i]).VirtualOffset, (int)((HeapMemory)memoryPointers.Pointers[i]).VirtualCount), noMerge, headerSize);
                     }
-                }
 
-                // Dealloc the array always.
-                memoryManager.DeAlloc(memoryPointers);
+                    if (dealloc)
+                    {
+                        // DeAlloc the memory again. This is done for unreliable channels that dont need the message after the initial send.
+                        for (int i = 0; i < memoryPointers.VirtualCount; i++)
+                        {
+                            memoryManager.DeAlloc(((HeapMemory)memoryPointers.Pointers[i]));
+                        }
+                    }
+
+                    // Dealloc the array always.
+                    memoryManager.DeAlloc(memoryPointers);
+                }
+            }
+            else
+            {
+                if (Logging.CurrentLogLevel <= LogLevel.Warning) Logging.LogWarning("Sending packet failed because the channel is not assigned");
             }
         }
     }
