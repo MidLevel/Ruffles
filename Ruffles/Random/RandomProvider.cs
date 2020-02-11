1dc03d169adddc2558847781bd69002c7b6b1757
Albin Corén <twoten@twoten.io>
2020-02-11 09:50:15 +0100

    fix: Made random provider thread safe

--- Ruffles/Random/RandomProvider.cs
+++ Ruffles/Random/RandomProvider.cs
@@ -1,24 +1,26 @@
-﻿using System;
-
-namespace Ruffles.Random
+﻿namespace Ruffles.Random
 {
     internal static class RandomProvider
     {
-        private static readonly System.Random random = new System.Random();
-        private static readonly byte[] randomBuffer = new byte[8];
+        private static readonly System.Random _random = new System.Random();
+        private static readonly byte[] _randomBuffer = new byte[8];
+        private static readonly object _randomLock = new object();
 
         internal static ulong GetRandomULong()
         {
-            random.NextBytes(randomBuffer);
+            lock (_randomLock)
+            {
+                _random.NextBytes(_randomBuffer);
 
-            return (((ulong)randomBuffer[0]) |
-                    ((ulong)randomBuffer[1] << 8) |
-                    ((ulong)randomBuffer[2] << 16) |
-                    ((ulong)randomBuffer[3] << 24) |
-                    ((ulong)randomBuffer[4] << 32) |
-                    ((ulong)randomBuffer[5] << 40) |
-                    ((ulong)randomBuffer[6] << 48) |
-                    ((ulong)randomBuffer[7] << 56));
+                return (((ulong)_randomBuffer[0]) |
+                        ((ulong)_randomBuffer[1] << 8) |
+                        ((ulong)_randomBuffer[2] << 16) |
+                        ((ulong)_randomBuffer[3] << 24) |
+                        ((ulong)_randomBuffer[4] << 32) |
+                        ((ulong)_randomBuffer[5] << 40) |
+                        ((ulong)_randomBuffer[6] << 48) |
+                        ((ulong)_randomBuffer[7] << 56));
+            }
         }
     }
 }
